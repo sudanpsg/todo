e771537e534adcf92feba17fc269d04c2b608766
sudanosg <sudanpsg@gmail.com>
2018-10-11 09:23:24 -0400

    my commit

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@
 
  app.post('/todos',(req,res)=>{
    console.log(req.body)
+   var todo = new Todo({
+     text:req.body.text
+   })
+   todo.save().then((doc)=>{
+     res.send(doc)
+   },(e)=>{
+     res.status(400).send(e)
+   })
  })
 
  app.listen(3000,()=>{
